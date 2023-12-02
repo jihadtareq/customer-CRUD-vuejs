85e233b1654a7461655a51b4b3647ebc187da1c9
JihadTareq <jihad@dooolab.com>
2023-12-02 17:35:15 +0200

    customer index

--- src/views/customers/CustomerIndex.vue
+++ src/views/customers/CustomerIndex.vue
@@ -1,3 +1,44 @@
 <script setup>
+  import useCustomers from '../../composables/customers';
+  import { onMounted } from 'vue';
+  const { customers,getCustomers} = useCustomers();
+
+  onMounted(()=>getCustomers());
 </script>
-<template></template>
+<template>
+    <div class="mt-12">
+        <div class="flex justify-end m-2 p-2">
+            <RouterLink :to="{name: 'customerCreate' }">New Customer</RouterLink>
+        </div>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            phone
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            email
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="customer in customers" :key="customer.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">{{ customer.name }}</td>
+                        <td class="px-6 py-4">{{ customer.phone }}</td>
+                        <td class="px-6 py-4">{{ customer.email }}</td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
