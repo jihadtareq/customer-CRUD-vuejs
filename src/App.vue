157b77e3c9c48b2e38f5930df9e61397f74afe5c
JihadTareq <jihad@dooolab.com>
2023-12-03 20:18:04 +0200

    touches

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
 
-    <div class="max-w-7xl mx-auto">
-      <nav class="p-2">
+    <!-- <div class="max-w-7xl mx-auto"> -->
+      <nav class="p-2 navBar">
         <RouterLink class="px-4 py-2 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white" to="/">Customer List</RouterLink>
       </nav>
-    </div>
+    <!-- </div> -->
   </header>
 
   <main class="max-w-7xl mx-auto min-h-screen">
